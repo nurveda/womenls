b5ceba9e4b3bfc231141b89eb2c12e3258258fd1
nurveda <utoglunurr@gmail.com>
2021-11-05 23:47:04 +0300

    added second line

--- src/test/java/nurClass.java
+++ src/test/java/nurClass.java
@@ -1,5 +1,6 @@
 public class nurClass {
     public static void main(String[] args) {
         System.out.println("this is my beautiful class");
+        System.out.println("added more line");
     }
 }
